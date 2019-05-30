775a2bd9a14b1fc2aa6c31a9f26900f6f3d6508c
chandan <chandan871@gmail.comm>
2019-05-30 12:53:31 +0530

    changes made in all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from '../app/home/home.component';
+import {IndexComponent} from '../app/index/index.component';
+import {LoginComponent} from '../app/login/login.component';
 
-const routes: Routes = [];
-
+/* const routes: Routes = [{path: '', component: IndexComponent},
+{path: '', component: HomeComponent,
+children: [{path: 'login', component: LoginComponent}]}
+]; */
+const routes: Routes = [
+{path: '', component: IndexComponent},
+{path: '', redirectTo: 'login', pathMatch: 'full'},
+{path: '', component: HomeComponent},
+{path: 'login', component: LoginComponent},
+{path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
+{
+  path: '',
+  redirectTo: '',
+  pathMatch: 'full'
+}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import { IndexComponent } from './index/index.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {LoginModal} from '../app/home/home.component';
+import {RegistrationModel} from '../app/home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { UsersService } from '../app/users.service';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    LoginComponent,
+    IndexComponent,
+    LoginModal,
+    RegistrationModel,
+    RegistrationComponent
   ],
+  entryComponents: [HomeComponent, LoginModal, RegistrationModel],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    ToastrModule.forRoot({timeOut: 10000, positionClass: 'toast-top-center', preventDuplicates: true}),
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    AppRoutingModule,
+    MatSelectModule,
+    MatDialogModule,
+    HttpClientModule,
+    NgxUiLoaderModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  /* declarations: [HomeComponent, LoginModal], */
+  providers: [UsersService],
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
